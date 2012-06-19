d254fe743c7f7dbabfaeb6ff9a57d97f65666412
Julian Pawlowski <julian.pawlowski@gmail.com>
2012-06-19 14:56:58 +0200

    Major upgrade to version 0.7

---
+++ config.v2/chroot_local-hooks/99-gdfdl_bootconf.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# GDFDL - A Development Framework for Debian live-build
+# Enable system services
+#
+# Copyright (c) 2012, Julian Pawlowski <jp@jps-networks.eu>
+# See LICENSE file for details.
+#
+
+# check each command return codes for errors
+#
+set -e
+
+###
+# Don't change this file directly. Create separate file with sort order after this file instead.
+###
+
+# General settings
+source /gdfdl.conf
+[ -f /gdfdl-custom.conf ] && source /gdfdl-custom.conf
+
+echo -e "\n###########################################################
+## GDFDL: System bootup configuration\n\n"
+
+echo -e "GDFDL: Enabling system services ...\n"
+update-rc.d gdfdl-init defaults 2>&1
+update-rc.d gdfdl-prompt defaults 2>&1
