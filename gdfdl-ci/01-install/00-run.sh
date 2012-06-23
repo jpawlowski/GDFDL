74408ee23d6bcfe174577dffc60ca80e754ee430
Julian Pawlowski <julian.pawlowski@gmail.com>
2012-06-23 12:51:07 +0200

    update CI scripts

--- gdfdl-ci/01-install/00-run.sh
+++ gdfdl-ci/01-install/00-run.sh
@@ -5,9 +5,14 @@ SELF="`readlink -f $0`"
 GDFDL_INSTALLER_BASEDIR_CI_00="`dirname ${SELF}`"
 GDFDL_INSTALLER_BASEDIR_CI="`dirname ${GDFDL_INSTALLER_BASEDIR_CI_00}`"
 GDFDL_INSTALLER_BASEDIR="`dirname ${GDFDL_INSTALLER_BASEDIR_CI}`"
+if [ -d "${GDFDL_INSTALLER_BASEDIR}/.ci" ]
+	then
+	echo "ERROR: CI directory '${GDFDL_INSTALLER_BASEDIR}/.ci' already exists. Run uninstaller to cleanup first."
+	exit 1
+fi
 mkdir "${GDFDL_INSTALLER_BASEDIR}/.ci"
 cd "${GDFDL_INSTALLER_BASEDIR}/.ci"
 
-GDFDL_BRANCH=""
+GDFDL_BRANCH="`git remote show origin | grep HEAD | cut -d " " -f 5`"
 
 GDFDL_INSTALLTYPE="ci" bash -ex "${GDFDL_INSTALLER_BASEDIR}/gdfdl-scripts/gdfdl-installer" "${GDFDL_BRANCH}" "${GDFDL_INSTALLER_BASEDIR}" "${GDFDL_INSTALLER_BASEDIR}"
--- gdfdl-ci/README.md
+++ gdfdl-ci/README.md
@@ -1,12 +1,13 @@
+# GDFDL
 Continuous Integration
 ======================
 
-Usage with CI systems like Jenkins to be described.
+Usage with CI systems like Jenkins to be descried.
 
 
-Copy&Paste to Jenkins/Hudson Execute shell field:
-./gdfdl-ci/00-uninstall/01-uninstall.sh
-./gdfdl-ci/01-install/01-uninstall.sh
-./gdfdl-ci/02-build/01-uninstall.sh
-./gdfdl-ci/03-QA/01-uninstall.sh
-./gdfdl-ci/04-deploy/01-deploy.sh
+Copy&Paste to Jenkins/Hudson Execute shell field after you have cloned the Git:
+./gdfdl-ci/00-uninstall/00-run.sh
+./gdfdl-ci/01-install/00-run.sh
+./gdfdl-ci/02-build/00-run.sh
+./gdfdl-ci/03-QA/00-run.sh
+./gdfdl-ci/04-deploy/00-run.sh
