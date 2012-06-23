32ecb2867626831216777aa4d051fc2854c9d1af
Julian Pawlowski <julian.pawlowski@gmail.com>
2012-06-23 13:27:54 +0200

    CI update

--- gdfdl-ci/01-install/00-run.sh
+++ gdfdl-ci/01-install/00-run.sh
@@ -5,14 +5,11 @@ SELF="`readlink -f $0`"
 GDFDL_INSTALLER_BASEDIR_CI_00="`dirname ${SELF}`"
 GDFDL_INSTALLER_BASEDIR_CI="`dirname ${GDFDL_INSTALLER_BASEDIR_CI_00}`"
 GDFDL_INSTALLER_BASEDIR="`dirname ${GDFDL_INSTALLER_BASEDIR_CI}`"
-if [ -d "${GDFDL_INSTALLER_BASEDIR}/.ci" ]
-	then
-	echo "ERROR: CI directory '${GDFDL_INSTALLER_BASEDIR}/.ci' already exists. Run uninstaller to cleanup first."
-	exit 1
-fi
-mkdir "${GDFDL_INSTALLER_BASEDIR}/.ci"
-cd "${GDFDL_INSTALLER_BASEDIR}/.ci"
+[ x"$1" == x"" ] && GDFDL_INSTALLER_DESTINATION="${GDFDL_INSTALLER_BASEDIR}/.ci" || GDFDL_INSTALLER_DESTINATION="$1"
+
+[ ! -d "${GDFDL_INSTALLER_DESTINATION}" ] && mkdir "${GDFDL_INSTALLER_DESTINATION}"
+cd "${GDFDL_INSTALLER_DESTINATION}"
 
 GDFDL_BRANCH="`git remote show origin | grep HEAD | cut -d " " -f 5`"
 
-GDFDL_INSTALLTYPE="ci" bash -ex "${GDFDL_INSTALLER_BASEDIR}/gdfdl-scripts/gdfdl-installer" "${GDFDL_BRANCH}" "${GDFDL_INSTALLER_BASEDIR}" "${GDFDL_INSTALLER_BASEDIR}"
+bash -ex "${GDFDL_INSTALLER_BASEDIR}/gdfdl-scripts/gdfdl-installer" "${GDFDL_BRANCH}" "${GDFDL_INSTALLER_BASEDIR}" "${GDFDL_INSTALLER_BASEDIR}" ci
