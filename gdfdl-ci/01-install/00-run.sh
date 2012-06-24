b4ab52b2a315fdf1270283215575f34e5b51cd9f
Julian Pawlowski <julian.pawlowski@gmail.com>
2012-06-24 20:55:45 +0200

    fix CI installer

--- gdfdl-ci/01-install/00-run.sh
+++ gdfdl-ci/01-install/00-run.sh
@@ -17,22 +17,22 @@ set -e
 
 SELF="`readlink -f $0`"
 GDFDL_BASEDIR_CI_STEP="`dirname ${SELF}`"
-GDFDL_BASEDIR_CI="`dirname ${GDFDL_INSTALLER_BASEDIR_CI_STEP}`"
-GDFDL_BASEDIR="`dirname ${GDFDL_INSTALLER_BASEDIR_CI}`"
-[ x"$1" == x"" ] && GDFDL_INSTALLER_DESTINATION="${GDFDL_INSTALLER_BASEDIR}/.ci" || GDFDL_INSTALLER_DESTINATION="$1"
+GDFDL_BASEDIR_CI="`dirname ${GDFDL_BASEDIR_CI_STEP}`"
+GDFDL_BASEDIR="`dirname ${GDFDL_BASEDIR_CI}`"
+[ x"$1" == x"" ] && GDFDL_DESTINATION="${GDFDL_BASEDIR}/.ci" || GDFDL_DESTINATION="$1"
 
 if [[ x"${GIT_BRANCH}" != x"" ]]
 	then
 	GDFDL_BRANCH="${GIT_BRANCH}"
 else
-	GDFDL_BRANCH="`cd "${GDFDL_INSTALLER_BASEDIR}"; git branch | cut -d " " -f 2`"
+	GDFDL_BRANCH="`cd "${GDFDL_BASEDIR}"; git branch | cut -d " " -f 2`"
 fi
 
-[ ! -d "${GDFDL_INSTALLER_DESTINATION}" ] && mkdir "${GDFDL_INSTALLER_DESTINATION}"
-cd "${GDFDL_INSTALLER_DESTINATION}"
+[ ! -d "${GDFDL_DESTINATION}" ] && mkdir "${GDFDL_DESTINATION}"
+cd "${GDFDL_DESTINATION}"
 
 # start the actual installer
-bash "${GDFDL_INSTALLER_BASEDIR}/gdfdl-scripts/gdfdl-installer" "${GDFDL_BRANCH}" "${GDFDL_INSTALLER_BASEDIR}" "${GDFDL_INSTALLER_BASEDIR}" ci
+bash "${GDFDL_BASEDIR}/gdfdl-scripts/gdfdl-installer" "${GDFDL_BRANCH}" "${GDFDL_BASEDIR}" "${GDFDL_BASEDIR}" ci
 
 cd - 2>&1 >/dev/null
 
