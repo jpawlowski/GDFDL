134a4c1c106e35f39511e14bec9890b831ce65bd
Julian Pawlowski <julian.pawlowski@gmail.com>
2012-06-24 20:35:00 +0200

    fix find usage

--- gdfdl-ci/01-install/00-run.sh
+++ gdfdl-ci/01-install/00-run.sh
@@ -39,7 +39,7 @@ cd - 2>&1 >/dev/null
 # if we find another script in the series, go on and run that
 # (but ignore 01-run.sh to avoid loops)
 #
-GDFDL_CI_NEXT="`find "${GDFDL_BASEDIR_CI_STEP}" -type f -maxdepth 1 -name '01-*.sh' | grep -v 01-run.sh`"
+GDFDL_CI_NEXT="`find "${GDFDL_BASEDIR_CI_STEP}" -maxdepth 1 -type f -name '01-*.sh' | grep -v 01-run.sh`"
 if [ x"${GDFDL_CI_NEXT}" != x"" ]
 	then
 	echo "NOTE: Next script '${GDFDL_CI_NEXT}' found, handing over now ..."
